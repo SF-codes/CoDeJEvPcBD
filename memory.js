4e9de899b4a4d94aff4670448994a70773b529dd
Shanis <felicissimo.shanis@gmail.com>
2022-09-10 16:50:41 -0400

    edits

--- memory.js
+++ memory.js
@@ -1,5 +1,7 @@
 export let memory = [
   "Hopping down the stairs together cause why not!",
   "That first long-but-not-long-enough hug in the light misty sunshower rain",
-  "",
+  "Leaning on the railing with you :p",
+  "The best, longest but not long enough hug",
+  "Level up hug!",
 ];
